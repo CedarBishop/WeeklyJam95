ba020b7b63d67ead338dcfbf3da767e9c327e744
CedarBishop <47874454+CedarBishop@users.noreply.github.com>
2019-05-05 14:14:02 +1000

    Added pause menu and main menu scene

---
+++ Assets/Scripts/PauseLogic.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseLogic : MonoBehaviour
+{
+    public Canvas pauseCanvas;
+    public string sceneName;
+
+    void Start()
+    {
+        pauseCanvas.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && pauseCanvas.gameObject.activeSelf)
+        {
+            Resume();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && pauseCanvas.gameObject.activeSelf == false)
+        {
+            Pause();
+        }
+    }
+
+    public void Resume ()
+    {
+        pauseCanvas.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Pause ()
+    {
+        pauseCanvas.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
