385645d52a8aa62113149726eebe100c62550e92
CedarBishop <47874454+CedarBishop@users.noreply.github.com>
2019-05-08 18:40:23 +1000

    Added Dialogue System and text to the game win and lose canvases

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    public Canvas dialogueCanvas;
+    public Text dialogueText;
+    public string[] sentences;
+    public float typingSpeed = 1;
+    public HeadChefLogic[] headChefLogics;
+    //public AudioClip buttonSound;
+
+    private int index;
+    private bool sentenceHasFinished;
+    private PlayerMovement playerMovement;
+    private AnimationInput animationInput;
+    private FoodSpawner foodSpawner;
+
+    void Start()
+    {
+        dialogueCanvas.gameObject.SetActive(true);
+        dialogueText.text = "";
+        StartCoroutine("Type");
+        foodSpawner = GetComponent<FoodSpawner>();
+        playerMovement = GameObject.Find("Player").GetComponent<PlayerMovement>();
+        animationInput = GameObject.Find("Player").GetComponent<AnimationInput>();
+        ScriptsAffectedByDialogue(false);
+    }
+
+    void Update ()
+    {
+        if (sentenceHasFinished)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                NextSentence();
+            }
+        }
+    }
+
+    IEnumerator Type ()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(typingSpeed * Time.deltaTime);
+        }
+        sentenceHasFinished = true;
+    }
+
+    void NextSentence ()
+    {
+        dialogueText.text = "";
+        sentenceHasFinished = false;
+        if (index < sentences.Length - 1)
+        {
+            index++;            
+            StartCoroutine("Type");
+        }
+        else
+        {
+            index = 0;
+            dialogueCanvas.gameObject.SetActive(false);
+            ScriptsAffectedByDialogue(true);
+        }        
+    }
+
+    void ScriptsAffectedByDialogue (bool answer)
+    {
+        foreach (var item in headChefLogics)
+        {
+            item.enabled = answer;
+        }
+        foodSpawner.enabled = answer;
+        playerMovement.enabled = answer;
+        animationInput.enabled = answer;
+    }
+}
